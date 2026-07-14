340ef7ed6318e877aae65a597fbc1c475da78ddd
Ricardo Iuji <138956575+di0x1@users.noreply.github.com>
2026-07-14 10:37:13 -0300

    Add files via upload

---
+++ src/main/java/br/com/axion/axion_academy/AxionAcademyApplication.java
@@ -0,0 +1,13 @@
+package br.com.axion.axion_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxionAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxionAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/axion/axion_academy/AxionAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axion.axion_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxionAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
